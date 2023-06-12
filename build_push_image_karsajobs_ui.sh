f478dbe4cf2af4402393f882d2b669a268d74cd3
manuyafsya <manuyafsya@gmail.com>
2023-06-12 15:39:01 +0700

    submit-project-dua

---
+++ build_push_image_karsajobs_ui.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Build Docker image dari Dockerfile
+docker build -t karsajobs-ui .
+
+# Mengubah nama image agar sesuai dengan format GitHub Packages
+docker tag karsajobs-ui ghcr.io/kecoax-code/a433-microservices/karsajobs-ui:latest
+
+# Login ke GitHub Packages
+docker login ghcr.io -u kecoax-code -p $CR_PAT
+
+# Mengunggah image ke GitHub Packages
+docker push ghcr.io/kecoax-code/a433-microservices/karsajobs-ui:latest
